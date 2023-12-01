e830c5cd0fc793d7de36cfa738cd20098b13f321
Herwiggg <michelot.alexy78@gmail.com>
2023-12-01 15:28:59 +0100

    last push

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: almichel <almichel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/25 16:07:37 by almichel          #+#    #+#             */
-/*   Updated: 2023/12/01 15:06:53 by almichel         ###   ########.fr       */
+/*   Created: 2023/12/01 15:12:31 by almichel          #+#    #+#             */
+/*   Updated: 2023/12/01 15:12:53 by almichel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 10
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: almichel <almichel@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/01 15:13:16 by almichel          #+#    #+#             */
+/*   Updated: 2023/12/01 15:17:14 by almichel         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+int	ft_strlen(char *str, int nb)
+{
+	int	i;
+
+	i = 0;
+	if (nb == 0)
+	{
+		while (str[i])
+			i++;
+	}
+	if (nb == 1)
+	{
+		while (str[i] && str[i] != '\n')
+			i++;
+	}
+	return (i);
+}
+
+void	ft_avoid_crushing(char *buf, char *line)
+{
+	int	i;
+
+	i = -1;
+	while (buf[++i] && buf[i] != '\n')
+		line[i] = buf[i];
+	line[i] = '\0';
+}
+
+void	ft_check_n_fractionned(char *str, char *dest, int nb)
+{
+	int	i;
+
+	i = -1;
+	if (nb == 0)
+	{
+		while (str[++i])
+			dest[i] = str[i];
+		dest[i] = '\0';
+	}
+	if (nb == 1)
+	{
+		while (str[++i] != '\n')
+			dest[i] = str[i];
+		dest[i] = '\n';
+		dest[++i] = '\0';
+	}
+}
+
+char	*ft_check_n(char *str, int *error)
+{
+	char	*dest;
+
+	if (found_newline(str) == 0)
+	{
+		dest = malloc((ft_strlen(str, 0) + 1) * sizeof(char));
+		if (!dest)
+		{
+			*error = 1;
+			return (NULL);
+		}
+		ft_check_n_fractionned(str, dest, 0);
+	}
+	else
+	{
+		dest = malloc((ft_strlen(str, 1) + 2) * sizeof(char));
+		if (!dest)
+		{
+			*error = 1;
+			return (NULL);
+		}
+		ft_check_n_fractionned(str, dest, 1);
+	}
+	return (dest);
+}
+
+char	*ft_clear_buff(char *buffer)
+{
+	int	i;
+
+	i = 0;
+	while (i < BUFFER_SIZE + 1)
+		buffer[i++] = '\0';
+	return (0);
+}
